8799d38b7da62fd91335e6f8f5bb2c56693b3aa1
roundcalicocat <alison.nakailackey@gmail.com>
2026-04-01 14:17:29 -0400

    remove redundant code

--- enrichments.py
+++ enrichments.py
@@ -1,7 +1,6 @@
 from pyspark.sql import functions as F
 
 from config import SETTINGS
-from event_types import PET_WEIGHT_RECORDED
 
 
 def find_cat_name(input_df):
@@ -9,6 +8,8 @@ def find_cat_name(input_df):
     Enriches only PET_WEIGHT_RECORDED events with a cat_name column based on
     weight ranges in config. Events with weights outside of known ranges
     are given the cat_name "Unknown"
+
+    prereq: input_df must be filtered to PET_WEIGHT_RECORDED events only
 
     output cols: input_df cols, cat_name
     """
@@ -20,11 +21,9 @@ def find_cat_name(input_df):
     return input_df.withColumn(
         "cat_name",
         F.when(
-            F.col("event_type") == PET_WEIGHT_RECORDED, 
-            F.when(
-                # default to "unknown" if weight can't be associated
-                # a cat stepping in once and immediately exiting can cause this
-                F.col("value").between(min_weight, max_weight), name)
-                .otherwise(F.lit("Unknown")) 
-            )
+            # default to "unknown" if weight can't be associated
+            # a cat stepping in once and immediately exiting can cause this
+            F.col("value").between(min_weight, max_weight), name)
+            .otherwise(F.lit("Unknown")
+        ) 
     )
